089a5be66f5fe67a9b6f900060dc29002ff06226
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-27 13:37:18 +0530

    Third

--- lib/view/flutter_today.dart
+++ lib/view/flutter_today.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-//import '../model/menu.dart';
-import '../model/navigation_controls.dart';
-import '../model/web_view_stack.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import '../model/source_of_web_view.dart';
+import '../controller/navigation_controller.dart';
 
 class FlutterToday extends StatefulWidget {
   const FlutterToday({Key? key}) : super(key: key);
@@ -21,13 +20,12 @@ class _FlutterTodayState extends State<FlutterToday> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter WebView'),
+        title: const Text('Flutter Today'),
         actions: [
-          NavigationControls(controller: controller),
-          //Menu(controller: controller),
+          NavigationController(navigationController: controller),
         ],
       ),
-      body: WebViewStack(controller: controller),
+      body: SourceOfWebView(webViewController: controller),
     );
   }
 }

526a6c2029e86dbc076934b55a3c46c3eb6f9995
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-30 07:15:21 +0530

    Dhuttteri

--- lib/controller/dashboard_home.dart
+++ lib/controller/dashboard_home.dart
@@ -8,10 +8,7 @@ import 'package:flutter_today/view/flutter_apps.dart';
 import 'package:flutter_today/view/intermediate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import '../view/first_page.dart';
 import '../view/home_page.dart';
-import '../view/second_page.dart';
-import '../view/third_page.dart';
 
 class DashBoardHome extends StatefulWidget {
   const DashBoardHome({
@@ -27,77 +24,12 @@ class _DashBoardHomeState extends State<DashBoardHome>
   final controller = Completer<WebViewController>();
   TabController? _tabController;
 
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    FirstPage(),
-    SecondPage(),
-    ThirdPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   final List<Tab> topTabs = <Tab>[
-    Tab(
-      iconMargin: const EdgeInsets.only(bottom: 10.0),
-      child: Text(
-        'HOME',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-    ),
-    Tab(
-      child: Text(
-        'BEGINNERS',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-    ),
-    Tab(
-      child: Text(
-        'INTERMEDIATE',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-    ),
-    Tab(
-      child: Text(
-        'FLUTTER APPS',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-    ),
-    Tab(
-      child: Text(
-        'DART',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-      ),
-    ),
+    const Tab(child: Text('HOME')),
+    const Tab(child: Text('BEGINNERS')),
+    const Tab(child: Text('INTERMEDIATE')),
+    const Tab(child: Text('FLUTTER APPS')),
+    const Tab(child: Text('DART')),
   ];
 
   @override
@@ -135,74 +67,31 @@ class _DashBoardHomeState extends State<DashBoardHome>
           key: _scaffoldKey,
           appBar: AppBar(
             title: Text(
-              'Flutter Today',
-              textAlign: TextAlign.center,
+              'FlutterToday',
               style: GoogleFonts.laila(
-                fontSize: 30.0,
+                fontSize: 35.0,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.deepPurple,
               ),
             ),
-            centerTitle: true,
-
-            //backgroundColor: Colors.grey[400],
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.pink,
-                    Colors.grey,
-                  ],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            elevation: 20,
-            titleSpacing: 60,
             bottom: TabBar(
-              isScrollable: true,
-              indicatorColor: Colors.red,
-              indicatorWeight: 10,
               controller: _tabController,
+              indicatorColor: Colors.black,
               tabs: topTabs,
+              isScrollable: true,
             ),
           ),
-          body: Column(
+          body: TabBarView(
+            controller: _tabController,
             children: [
-              TabBarView(
-                controller: _tabController,
-                children: [
-                  /// all categories displayed on tabs
-                  ///
-                  HomePage(webViewController: controller),
-                  Beginner(webViewController: controller),
-                  Intermediate(webViewController: controller),
-                  FlutterApps(webViewController: controller),
-                  DartHome(webViewController: controller),
-                ],
-              ),
-              _widgetOptions.elementAt(_selectedIndex),
+              /// all categories displayed on tabs
+              ///
+              HomePage(webViewController: controller),
+              Beginner(webViewController: controller),
+              Intermediate(webViewController: controller),
+              FlutterApps(webViewController: controller),
+              DartHome(webViewController: controller),
             ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.business),
-                label: 'Business',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.school),
-                label: 'School',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.amber[800],
-            onTap: _onItemTapped,
           ),
         ),
       ),
